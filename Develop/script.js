830130b7d63425f108f05d53521671b2d2589ddd
Alisha <panday.alisha90@gmail.com>
2020-11-22 17:43:49 +0800

    Generate password function using user prompt, validation and loop

--- Develop/script.js
+++ Develop/script.js
@@ -1,4 +1,5 @@
 // Assignment Code
+//declared a variable generateBtn with query selector based on the id generate
 var generateBtn = document.querySelector("#generate");
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
@@ -28,7 +29,7 @@ generateBtn.addEventListener("click", writePassword);
    userLength = parseInt(prompt("How many Length of characters for password? Type num: "))
 
 
-   //While loop for user input validation to check if its under the criteria of 8-128 char
+   //While loop for user input validation to check if  under the criteria of 8-128 char and not empty
    
    while (!userLength |userLength < 8 || userLength > 128){
      alert ("This needs a value between 8-128 character & cannot be empty");
@@ -43,7 +44,7 @@ generateBtn.addEventListener("click", writePassword);
      var confirmNum = confirm("Do you want Number in your password?");
      var confirmSymbol = confirm("Do you want Symbol in your password?");
 
-    //User validation for selecting at least one criteria
+    //User input validation to select atleast one character type
 
     while (confirmUpper === false && confirmLower === false && confirmNum=== false && confirmSymbol=== false) {
         alert("You must at least select one character type");
@@ -77,17 +78,21 @@ generateBtn.addEventListener("click", writePassword);
        var choices= choices.concat(characters.symbol);
      }
 
-    
      console.log(choices);
 
-     
-   
 
+     //Create variable to store random password generated according to user choices of criteria
+
+      var randomPassword = '';
+    
+     //For loop to loop through the entered length of times and add the password char types
+      for (i = 0; i < userLength; i++){
+         var randomPassword = randomPassword + choices [Math.floor(Math.random()*choices.length)];
+          console.log(randomPassword);    
+        }
+        return randomPassword;
    
  }
-
-
-
 
 
 
